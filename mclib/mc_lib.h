ace6989b65380826c890de2f4b24efa615b9a7c2
Dallas DeBruin <ddebruin@c2at.com>
2022-04-29 10:59:46 -0400

    Initial Skeleton

---
+++ mclib/mc_lib.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include "libalg.h"
+
+class Mesh {
+    public:
+        bool export(std::string filename);
+        bool add_quad(double3 
+    
+    protected:
+    
+};
