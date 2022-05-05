1ce19841154843cc799860734557e082c8ce8235
ExeVirus <ddsallad@gmail.com>
2022-05-04 22:44:18 -0400

    Halfway done with axis_aligned_rectangle

--- lua_api/lib/common_shape_functions.lua
+++ lua_api/lib/common_shape_functions.lua
@@ -40,3 +40,36 @@ shapes.quad = function(bottom_left_corner, top_left_corner, top_right_corner, gr
         group
     )
 end
+
+shapes.axis_aligned_rectangular_prism = function(bottom_left_corner, dimensions, g1, g2, g3, g4, g5, g6)
+    vector.validate_vector(bottom_left_corner)
+    vector.validate_vector(dimensions)
+    local function check_group(group)
+        if type(group) ~= "number" then shapes.error("4th arg: expected number, got "..type(group)) end
+    end
+
+    check_group(g1)
+    check_group(g2)
+    check_group(g3)
+    check_group(g4)
+    check_group(g5)
+    check_group(g6)
+--      .+-top--+
+--    .' |    .'|
+--   +---+--+'  |-back
+--   |   |  |   |
+--   |  ,+--+---+
+--   |.'    | .' 
+--   +-front+'  right -->
+
+    -- blf == bottom, left, front
+    blf = bottom_left_corner
+    blb = bottom_left_corner; blb.z = blb.z - dimensions.z
+    brf = bottom_left_corner; brf.x = brf.x + dimensions.x
+    brb = 
+    tlf = 
+    tlb = 
+    trf = 
+    trb = 
+
+end
